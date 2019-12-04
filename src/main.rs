3d88311e5781ebe9142a5cf63d0522f3375509c9
Tim Lucca <tim.lucca88@gmail.com>
2019-12-03 17:54:30 -0700

    changed to exhaustive search for paths

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 // This program determines profitable cycles from exchanging currencies
-
-use std::{fs, collections::HashMap, io::{self, Read}, f64};
+use std::{fs, collections::HashMap, io, f64};
 
 // Reads "data.txt" and returns the number of currencies and the data as a vector
 fn read_data() -> (usize, Vec<String>) {
@@ -22,9 +21,13 @@ fn read_data() -> (usize, Vec<String>) {
 // which will be they key for the indicies in the graph
 fn build_graph(n: usize, arr: Vec<String>) -> (HashMap<String, usize>, Vec<Vec<f64>>) {
     let mut currency_map: HashMap<String, usize> = HashMap::new();
-    let mut graph = vec![vec![1.0; n]; n];
-
+    let mut graph = vec![vec![0.0; n]; n];
     let mut currency_index: usize = 0;
+
+    // Set diag weights to 1
+    for i in 0..graph.len() {
+        graph[i][i] = 1.0;
+    }
 
     for i in (0..arr.len()).step_by(3) {
         if !currency_map.contains_key(&arr[i]) {
@@ -44,64 +47,88 @@ fn build_graph(n: usize, arr: Vec<String>) -> (HashMap<String, usize>, Vec<Vec<f
     return (currency_map, graph);
 }
 
-// graph traversal to check if negative cycle exists and finds the cycle
-fn neg_cycles(graph: Vec<Vec<f64>>, start: usize) -> (Vec<usize>) {
-    let mut dist = vec![f64::MAX; graph.len()];
-    dist[start] = 0.0;
+// An exhaustive search to traverse all cycles from a starting point
+// returns the most profitable path and profit multiplier
+fn exhaust(profit: f64, start: usize, current: usize, graph: Vec<Vec<f64>>, visited: Vec<bool>, path: Vec<usize>) -> (Vec<usize>, f64) {
+    let mut updated_path = path.clone();
+    updated_path.push(current);
+    if visited[current] {
+        if current == start {
+            return (path, profit);
+        } else {
+            updated_path.push(start);
+            return (updated_path, profit * graph[current][start]);
+        }
+    }
+    let mut updated_visited = visited.clone();
+    updated_visited[current] = true;
+    let mut best_profit = 1.0;
+    let mut best_path = Vec::new();
+    for i in 0..graph.len() {
+        let ret = exhaust(profit * graph[current][i], start, i, graph.clone(), updated_visited.clone(), updated_path.clone());
+        if ret.1 > best_profit {
+            best_profit = ret.1;
+            best_path = ret.0;
+        }
+    }
+    return (best_path, best_profit);
+}
 
-    for _ in 0..graph.len()-1 {
-        for m in 0..graph.len() {
-            for n in 0..graph.len() {
-                dist[n] = dist[n].min(dist[m] + graph[m][n]);
+// Displays the best cycle from the given starting currency and profits if 1000 of the currency is invested
+fn show_best(currency: usize, graph: Vec<Vec<f64>>, n: usize, map: HashMap<String, usize>) {
+    let best = exhaust(1.0, currency, currency, graph, vec![false; n], Vec::new());
+    let mut c = "";
+    println!("");
+    for i in 0..best.0.len() {
+        for (k, v) in &map {
+            if v==&best.0[i] {
+                print!("{} ", k);
+                if i<best.0.len()-1 {
+                    print!("--> ");
+                }
+                if i == 0 {
+                    c = k;
+                }
             }
         }
     }
-    println!("V-1 times: {:?}", dist);
-
-    let mut dist2 = dist.clone();
-    for m in 0..graph.len() {
-        for n in 0..graph.len() {
-            dist2[n] = dist2[n].min(dist2[m] + graph[m][n]);
-        }
-    }
-    println!("V times: {:?}", dist2);
-
-    for i in 0..dist.len() {
-        if dist[i] != dist2[i] {
-            println!("Negative cycle detected.");
-            break;
-        }
-    }
-
-    return vec![];
+    println!("\nProfit if starting with 1000 {}'s: {}\n", c, 1000.0 * best.1);
 }
 
 fn main() {
+    // Read in the data
+    // Parses it into 'n', the number of currencies
+    // and 'arr', the vector of all of the data following 'n'
     let (n, arr) = read_data();
 
     if n < 1 {
         println!("Could not find the number of currencies\nEnding...");
         return;
     }
-    println!("# of currencies: {}", n);
-    
-    for i in 0..arr.len() {
-        print!("{}  ", arr[i]);
-        if (i+1)%3 == 0 {
-            println!();
-        }
-    }
 
+    // Builds a graph based on the data given
+    // 'map' is the list of curriencies and their indicies in the graph
     let (map, graph) = build_graph(n, arr);
 
-    // Transform the graph into a negative ln representation of the edges
-    let mut ln_graph = vec![vec![0.0; n]; n];
-    for i in 0..n {
-        for j in 0..n {
-            ln_graph[i][j] = -f64::ln(graph[i][j]);
+    // Main loop for user input
+    loop {
+        print!("Enter the corresponding number for the currency you want to start with, type 'q' to quit\n(");
+        for (k, v) in &map {
+            print!("{}: {}, ", k, v);
         }
+        println!(")");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Unable to read input");
+        if input.trim() == "q" {
+            break;
+        }
+
+        match input.trim().parse::<usize>() {
+            Ok(n) => (show_best(n, graph.clone(), graph.len(), map.clone())),
+            Err(e) => println!("Invalid input: {}", e),
+        }
+        
     }
-    // println!("{:?}", ln_graph);
-    let start: usize = 0;
-    let order = neg_cycles(ln_graph, start);
+    
 }
